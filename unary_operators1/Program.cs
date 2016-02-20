b4278cd3b690ddb4868ab520c4c84ed41f939186
AlexBennett <alexanderbondbennett@gmail.com>
2016-02-20 13:55:36 -0500

    started working with operators in c#

---
+++ unary_operators1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace unary_operators1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int unary = 0;
+			int preIncrement;
+			int preDecrement;
+			int postIncrement;
+			int postDecrement;
+			int positive;
+			int negative;
+			sbyte bitNot;
+			bool logNot;
+
+			preIncrement = ++unary;
+			Console.WriteLine("pre-Increment: {0}", preIncrement);
+
+			preDecrement = --unary;
+			Console.WriteLine("pre-Decrement: {0}", preDecrement);
+		}
+	}
+}
