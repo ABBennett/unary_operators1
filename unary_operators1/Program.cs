92e736d4624d0b80105b7847efbb9993832d4358
AlexBennett <alexanderbondbennett@gmail.com>
2016-02-20 14:17:10 -0500

    more work with imcrements and logic

--- unary_operators1/Program.cs
+++ unary_operators1/Program.cs
@@ -21,6 +21,28 @@ namespace unary_operators1
 
 			preDecrement = --unary;
 			Console.WriteLine("pre-Decrement: {0}", preDecrement);
+
+			postDecrement = unary--;
+			Console.WriteLine("Post-Decrement: {0}", postDecrement);
+
+			postIncrement = unary++;
+			Console.WriteLine("Post-Increment: {0}", postIncrement);
+
+			Console.WriteLine("Final Value of Unary: {0}", unary);
+
+			positive = -postIncrement;
+			Console.WriteLine("Positive: {0}", positive);
+
+			negative = +postIncrement;
+			Console.WriteLine("Negative: {0}", negative);
+
+			bitNot = 0;
+			bitNot = (sbyte)(~bitNot);
+			Console.WriteLine("Bitwise Not: {0}", bitNot);
+
+			logNot = false;
+			logNot = !logNot;
+			Console.WriteLine("Logical Not: {0}", logNot);
 		}
 	}
 }
